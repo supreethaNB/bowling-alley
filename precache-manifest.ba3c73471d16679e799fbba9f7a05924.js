cdddbbd092df7934b89cb6cb82c635e0ef65ed33
Supreetha Bhushan <supreetha.nb@gmail.com>
2022-04-08 19:42:41 +0530

    Updates

---
+++ precache-manifest.ba3c73471d16679e799fbba9f7a05924.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5b024bd4fbe790b7252",
+    "url": "%7BsupreethaNB%7D.github.io/static/css/main.51691509.chunk.css"
+  },
+  {
+    "revision": "c5b024bd4fbe790b7252",
+    "url": "%7BsupreethaNB%7D.github.io/static/js/main.5c63de40.chunk.js"
+  },
+  {
+    "revision": "812e7ac617183c656746",
+    "url": "%7BsupreethaNB%7D.github.io/static/js/runtime~main.3deef1fd.js"
+  },
+  {
+    "revision": "0b7892435016625f4bf2",
+    "url": "%7BsupreethaNB%7D.github.io/static/css/2.4f19d8d0.chunk.css"
+  },
+  {
+    "revision": "0b7892435016625f4bf2",
+    "url": "%7BsupreethaNB%7D.github.io/static/js/2.0b9b3ca0.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "%7BsupreethaNB%7D.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "%7BsupreethaNB%7D.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "%7BsupreethaNB%7D.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "%7BsupreethaNB%7D.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "%7BsupreethaNB%7D.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "4e20143e496157887dfafd1cd26823d9",
+    "url": "%7BsupreethaNB%7D.github.io/index.html"
+  }
+];
